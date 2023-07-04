6dc34771d0acee1a938e258134cab54530a59ca4
perpetual-education <admin@perpetual.education>
2023-07-04 14:53:56 -0700

    Add AuthorizedStatus component

---
+++ src/components/__tests__/AuthorizedStatus.test.js
@@ -0,0 +1,8 @@
+import { describe, test, expect } from 'vitest';
+
+describe('AuthorizedStatus', function () {
+  test('renders component', function () {
+    // but how to "mount" it...
+    expect(true).toBe(true);
+  });
+});
