b28458660df553cb6fa391150c0731b5b3c33320
perpetual-education <admin@perpetual.education>
2023-07-04 12:55:05 -0700

    Add sanity test to prove tests are running

---
+++ src/components/__tests__/sanity-test.test.js
@@ -0,0 +1,9 @@
+import { test } from 'vitest';
+
+test('sanity test', () => {
+  return;
+});
+
+// if you return true or false , it still "passes" - so, we'd better find a better way to talk about this
+
+// note coverate-v8 doesn't work on StackBlitz
