126e77dfcc66edbeeb6d2c3b86bead916824eddd
perpetual-education <admin@perpetual.education>
2023-07-04 15:27:45 -0700

    Show example of shallowMount

--- src/components/__tests__/AuthorizedStatus.test.js
+++ src/components/__tests__/AuthorizedStatus.test.js
@@ -1,12 +1,12 @@
 import { describe, test, expect } from 'vitest';
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 
 import AuthorizedStatus from '../AuthorizedStatus.vue';
 
 describe('AuthorizedStatus.vue', function () {
-  test('renders component', function () {
-    const wrapper = mount(AuthorizedStatus);
+  test('renders component s', function () {
+    const wrapper = shallowMount(AuthorizedStatus);
     const p = wrapper.get('p');
-    expect(p.text()).toBe('Hello');
+    expect(p.text()).toContain('Hello');
   });
 });
