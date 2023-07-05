5427a095f765c97052eecd042e6cecbe59768341
sheriffderek <derek@sheriffderek.consulting>
2023-07-04 20:06:35 -0700

    Save initial progress bar tests

--- src/components/__tests__/ProgressBar.test.js
+++ src/components/__tests__/ProgressBar.test.js
@@ -4,10 +4,30 @@ import { shallowMount } from "@vue/test-utils";
 import ProgressBar from "@/components/ProgressBar.vue";
 
 describe("ProgressBar.vue", () => {
-	test("displays the bar when start is called", () => {
+	test("displays the bar when start is called", async () => {
 		const wrapper = shallowMount(ProgressBar);
 		expect(wrapper.classes()).toContain("hidden");
-		wrapper.vm.start();
+		await wrapper.vm.start();
 		expect(wrapper.classes()).not.toContain("hidden");
+	});
+
+	test("sets the bar to 100% when finished is called", async () => {
+		const wrapper = shallowMount(ProgressBar);
+		// await wrapper.vm.start();
+		await wrapper.vm.finish();
+		expect(wrapper.element.style.width).toBe("100%");
+	});
+
+	test("hides the bar when finish is called", async () => {
+		const wrapper = shallowMount(ProgressBar);
+		await wrapper.vm.finish();
+		expect(wrapper.classes()).toContain("hidden");
+	});
+
+	test("should reset the width when start is called", async () => {
+		const wrapper = shallowMount(ProgressBar);
+		// await wrapper.vm.finish();
+		await wrapper.vm.start();
+		expect(wrapper.element.style.width).toBe("0%");
 	});
 });
